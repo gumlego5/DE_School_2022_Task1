538a744171c7808fc2bfa5c516b446bb634bb414
Oleg <gumlego@gmail.com>
2022-03-30 21:50:12 +0300

    Added docker's stages

--- README.md
+++ README.md
@@ -1,16 +1,52 @@
 # DE_School_2022_Task1
 First task for DE_School_2022
 
-Run:                            docker-compose up --build
-Go to app's console:            docker attach test-app-1
-                                type "sd"
-                You're in the menu
-Go to db's console:             docker exec -it test-db-1 /bin/bash
-                                psql -d database -U username -W -h db
-                                secret
-                You're in the db's console
-                                SELECT * FROM heroes;
-Go to app's log.txt:            docker exec -it test-app-1 /bin/bash
-                                cat log.txt
-                            
+Tables:
+    1) heroes - Store heroes information: id,side,name,birthday year, magic(1,0), power(int), moto relates to heroes_motos, 
+        heroes_histories relates to heroes_histories, battle relation to battles
+    2) heroes_motos - Store heroes motos: id, hero_id, moto_id, moto
+    3) heroes_histories - Store heroes hostories: id, hero_id, story, heroes relates to heroes
+    4) battles - id, hero_1_id, hero_1_moto_id, hero_2_id, hero_2_moto_id, winner
 
+How to start project:
+1 Go to /services/ folder
+
+2.1 DEBUG:
+    Run:                            docker-compose up --build --force-recreate
+
+    Access to app's menu:           
+                                    docker attach app
+                                    press Enter for menu
+                    You're in the menu
+
+    Access to db's console:         
+                                    docker exec -it database /bin/bash
+                                    psql -d database -U username -W -h db
+                                    secret
+                    You're in the db's console
+                                    SELECT * FROM heroes;
+
+    Access to app's logs:            
+                                    docker exec -it database /bin/bash
+                                    cat log.txt
+                                    cat user_actions.txt
+
+2.2 PROD:
+    Run:                            docker-compose -f docker-compose_prod.yml up --build --force-recreate
+
+    Access to app's menu:           
+                                    docker attach app_prod
+                                    press Enter for menu
+                    You're in the menu
+
+    Access to db's console:         
+                                    docker exec -it database_prod /bin/bash
+                                    psql -d database -U username -W -h db
+                                    secret
+                    You're in the db's console
+                                    SELECT * FROM heroes;
+
+    Access to app's logs:            
+                                    docker exec -it database_prod /bin/bash
+                                    cat log.txt
+                                    cat user_actions.txt
--- services/app/app.py
+++ services/app/app.py
@@ -1,51 +1,26 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql.expression import func, select
-from datetime import date
-from sqlalchemy.schema import DropTable
-from sqlalchemy.ext.compiler import compiles
+import os
 import logging
-from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey, Boolean
-from sqlalchemy.orm import relationship, backref
+from time import sleep
+from datetime import date
 from random import choice
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer, Date, Table, ForeignKey, Boolean
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker,relationship, backref
+from sqlalchemy.schema import DropTable
+from sqlalchemy.sql.expression import func, select
 
+sleep(10)
 
-def_logger = logging.getLogger(__name__)
-def_logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-file_handler = logging.FileHandler("log.txt")
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(formatter)
-stream_handler = logging.StreamHandler()
-stream_handler.setLevel(logging.INFO)
-stream_handler.setFormatter(formatter)
-def zerocut(record): # filters battle's draw
-    if record.msg[-1] != "0":
-        return True
-logfilter = logging.Filter()
-logfilter.filter = zerocut
+PROD_STATUS = int(os.environ['PROD_STATUS'])
 
-def_logger.addFilter(logfilter)
-def_logger.addHandler(file_handler)
-def_logger.addHandler(stream_handler)
+Base = declarative_base()
 
-def_logger.debug("app.py starts")
+#Tables configuration
 @compiles(DropTable, "postgresql")
 def _compile_drop_table(element, compiler, **kwargs):
     return compiler.visit_drop_table(element) + " CASCADE"
-
-db_name = 'database'
-db_user = 'username'
-db_pass = 'secret'
-db_host = 'db'
-db_port = '5432'
-
-# Settings as shown in docker-compose.yml
-engine = create_engine('postgresql+psycopg2://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name))
-def_logger.info("Connection with database is established")
-Session = sessionmaker(bind=engine)
-Base = declarative_base()
 
 battle_hero = Table('battle_hero_association', Base.metadata,
     Column('battle_id', ForeignKey('battles.id')),
@@ -153,7 +128,9 @@ class Battle(Base):
         else:
             self.winner = 2
         print(self)
-        
+#Tables configuration end
+
+#Scripts configuration
 def add_hero(side:str,name:str,date,magic:bool,power:int):
     session.add(Hero(side,name, date,magic,power))
     session.commit()
@@ -207,14 +184,156 @@ def show_heroes():
     for each in query:
         print(each)
 
+#Initial script with default data
+def initiates():
+    def_logger.info("initiates() is running")
+    # 4 - create heroes
+    add_hero("Stark","Jon Snow", date(280, 1, 1),False,10)
+    add_history(1, "Jon Snow is the bastard son of Eddard Stark, Lord of Winterfell. He has five half-siblings: Robb, Sansa, Arya, Bran, and Rickon Stark. Unaware of the identity of his mother, Jon was raised at Winterfell. At the age of fourteen, he joins the Night's Watch, where he earns the nickname Lord Snow.")
+    add_hero("Targaryen","Daenerys Targaryen", date(281, 1, 1),True,15)
+    add_history(2,"Daenerys is in her early teens living in exile in Essos. She remains dependent on her abusive older brother, Viserys, and is forced to marry Dothraki horselord Khal Drogo in exchange for an army for Viserys, who wishes to return to Westeros and recapture the Iron Throne.")
+    add_hero("Lannister","Joffrey Baratheon", date(282, 1, 1),False,2)
+    add_history(3,"Prince Joffrey Baratheon is known to the Seven Kingdoms as the eldest son and heir of King Robert I Baratheon and Queen Cersei Lannister. A member of House Baratheon of King's Landing, his siblings are Princess Myrcella and Prince Tommen.")
+    add_hero("Stark","Arya Stark", date(287,1,1),True,13)
+    add_history(4, "Princess Arya Stark is the third child and second daughter of Lord Eddard Stark and his wife, Lady Catelyn Stark. She is the sister of the incumbent Westerosi monarchs, Sansa, Queen in the North, and Brandon, King of the Andals and the First Men.")
+    add_hero("Targaryen", "Aerys II Targaryen", date(240,1,1), False, 6)
+    add_history(5,"King Aerys II Targaryen, commonly called \"the Mad King\", was the sixteenth member of House Targaryen to rule from the Iron Throne. Although his rule began benevolently, he succumbed to the madness caused by his incestuous lineage, and was eventually deposed by Lord Robert Baratheon in a civil war.")
+    add_hero("Lannister","Tywin Lannister", date(234,1,1), False, 11)
+    add_history(6,"Lord Tywin Lannister was the head of House Lannister, Lord of Casterly Rock, Warden of the West, Lord Paramount of the Westerlands, Hand of the King for three different kings, and Protector of the Realm. He was the father of Cersei, Jaime, and Tyrion Lannister, and sole grandfather of the incest-born Joffrey, Myrcella, and Tommen Baratheon.")
+    add_hero("Lannister","Jaime Lannister",date(261,1,1),False,8)
+    add_history(7,"Ser Jaime Lannister was the elder son of Lord Tywin Lannister, younger twin brother of Queen Cersei Lannister, and older brother of Tyrion Lannister. He was involved in an incestuous relationship with Cersei, and unknown to most, he was the biological father of her three bastard children, Joffrey, Myrcella, and Tommen, as well as her unborn child.")
+    add_hero("Stark", "Eddard Stark",date(263,1,1), False, 5)
+    add_history(8,"Lord Eddard Stark, also known as Ned Stark, was the head of House Stark, the Lord of Winterfell, Lord Paramount and Warden of the North, and later Hand of the King to King Robert I Baratheon. He was the older brother of Benjen, Lyanna and the younger brother of Brandon Stark. He is the father of Robb, Sansa, Arya, Bran, and Rickon by his wife, Catelyn Tully, and uncle of Jon Snow, who he raised as his bastard son.")
+    add_hero("Targaryen", "Drogo",date(273,1,1),False, 9)
+    add_history(9,"Khal Drogo was a chieftain of a Dothraki khalasar. He was often referred to as \"The Great Khal.\" He is also the namesake of the last living dragon in existence, Drogon, who was the personal mount of his late widow.")
+
+    # 4 - create moto
+    add_moto(1,"If I Fall, Don't Bring Me Back")
+    add_moto(1,"I Do Know Some Things...")
+    add_moto(1,"You All Crowned Me Your King. I Never Wanted It...")
+    add_moto(2,"They can live in my new world, or they can die in their old one")
+    add_moto(2,"I will answer injustice with justice")
+    add_moto(2,"My reign has just begun")
+    add_moto(3,"I cannot abide the wailing of women")
+    add_moto(3,"I am the king! I will punish you!")
+    add_moto(3,"Will you forgive me for my rudeness?")
+    add_moto(4,"Fear cuts deeper than swords")
+    add_moto(4,"Not today")
+    add_moto(4,"A girl has no name")
+    add_moto(5,"I want him dead, the traitor.")
+    add_moto(5,"Burn them all! BURN THEM ALL!")
+    add_moto(5,"I want his head, you'll bring me his head, or you'll burn with all the rest.")
+    add_moto(6,"A Lion Doesn't Concern Himself With The Opinions Of The Sheep")
+    add_moto(6,"Why Is He Still Alive?")
+    add_moto(6,"You Really Think A Crown Gives You Power?")
+    add_moto(7, "By What Right Does The Wolf Judge The Lion?")
+    add_moto(7, "The Things I Do For Love.")
+    add_moto(7, "People Have Been Swinging At Me For Years, But They Always Seem To Miss...")
+    add_moto(8, "The winters are hard but the Starks will endure. We always have.")
+    add_moto(8, "We've come to a dangerous place. We can't afford to fight a war amongst ourselves.")
+    add_moto(8, "The man who passes the sentence should swing the sword")
+    add_moto(9, "You are no king.")
+    add_moto(9, "A Crown For A King!")
+    add_moto(9, "No!")
+
+    for i in range(10):
+        add_random_battle()
+
+ #Logger configuration      
+def_logger = logging.getLogger(__name__)
+def_logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
+file_handler = logging.FileHandler("log.txt")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.INFO)
+stream_handler.setFormatter(formatter)
+def_logger.addHandler(file_handler)
+def_logger.addHandler(stream_handler)
+def zerocut(record): # filters battle's draw
+    if record.msg[-1] != "0":
+        return True
+logfilter = logging.Filter()
+logfilter.filter = zerocut
+def_logger.addFilter(logfilter)
+
+def_logger.info(f"PROD VALUE = {PROD_STATUS} ")
+def_logger.info(f"START")
+
+#Connection config - Settings as shown in docker-compose.yml
+db_name = 'database'
+db_user = 'username'
+db_pass = 'secret'
+db_host = 'db'
+db_port = '5432'
+
+engine = create_engine('postgresql+psycopg2://{}:{}@{}:{}/{}'.format(db_user, db_pass, db_host, db_port, db_name))
+def_logger.info("Connection with database is established")
+Session = sessionmaker(bind=engine)
+session = Session()
+
 # 2 - generate database schema
-Base.metadata.drop_all(engine)
+if PROD_STATUS == 0:
+    Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
 
 def_logger.info("Tables are created")
 
-session = Session()
+if PROD_STATUS == 0:
+    initiates()
+    def_logger.info("Initilization completed")
+else:
+    def_logger.info("Initilization isn't required")
 
-def_logger.debug("Main script is over")
+# Menu
+def printmenu():
+    print(
+        f"Available options: \n"
+        f"1. Add hero \n"
+        f"2. Add moto \n"
+        f"3. Add history \n"
+        f"4. Add battle \n"
+        f"5. Add random battle \n"
+        f"6. Delete Hero \n"
+        f"7. Show All Heroes \n"
+    )
 
-import menu
+def_logger.debug("Menu started")
+    
+def_logger.removeHandler(file_handler)
+file_handler = logging.FileHandler("user_actions.txt")
+def_logger.addHandler(file_handler)
+
+while True:
+    printmenu()
+    s = input()
+    try:
+        s = int(s)
+    except ValueError:
+        pass
+    if type(s) == int:
+        if s in range(1,8):
+            if s == 1:
+                add_hero(input("Side: "),input("Name: "),date(int(input("Year of birth: ")),1,1),int(input("Magic (1,0): ")),int(input("Power: ")))
+                def_logger.info("User added a hero")
+            if s == 2:
+                add_moto(int(input("hero_id: ")),input("moto: "))
+                def_logger.info("User added a moto")
+            if s == 3:
+                add_history(int(input("hero_id: ")),input("history: "))
+                def_logger.info("User added a history")
+            if s == 4:
+                add_battle(int(input("hero1_id: ")),int(input("hero_id: ")))
+                def_logger.info("User added a battle")
+            if s == 5:
+                add_random_battle()
+                def_logger.info("User added a random battle")
+            if s == 6:
+                delete_hero(int(input("hero_id: ")))
+                def_logger.info("User deleted a hero")
+            if s == 7:
+                show_heroes()
+            print("Done")
+
+    
